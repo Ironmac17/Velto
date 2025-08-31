1815f3ebcb8f61c5fd5665c4cc3b30e5b74dd297
Ironmac17 <nimish4agrawal@gmail.com>
2025-08-31 23:07:34 +0530

    Backend basics

--- server/server.js
+++ server/server.js
@@ -4,10 +4,12 @@ const cors=require("cors");
 const path=require("path");
 const app=express();
 const connectDB=require("./config/db");
+const authRoutes=require("./routes/authRoutes");
+
 
 app.use(
     cors({
-        origin: process.env.CLIENT.URL||"*",
+        origin: process.env.CLIENT_URL||"*",
         methods:["GET","POST","PUT","DELETE"],
         allowedHeaders:["Content-Type","Authorization"]
     })
@@ -15,6 +17,8 @@ app.use(
 
 app.use(express.json());
 connectDB();
+
+app.use("/api/v1/auth" ,authRoutes);
 
 const PORT=process.env.PORT||5000;
 app.listen(PORT,()=>
