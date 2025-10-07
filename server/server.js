c121b165f894326913bce32e8024a40864008ff9
Ironmac17 <nimish4agrawal@gmail.com>
2025-10-07 18:27:45 +0530

    Auth api

--- server/server.js
+++ server/server.js
@@ -18,6 +18,7 @@ app.use(
 connectDB();
 app.use(express.json());
 app.use("/api/v1/auth", authRoutes);
+app.use('/uploads', express.static(path.join(__dirname,'uploads')));
 
 const PORT=process.env.PORT || 5000;
 app.listen(PORT,()=> console.log(`Server running on port ${PORT}`));
