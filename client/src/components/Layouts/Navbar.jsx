78f7148b446454c008beefedc6311dd05716cf96
Ironmac17 <nimish4agrawal@gmail.com>
2025-10-09 16:04:00 +0530

    Navbar

---
+++ client/src/components/Layouts/Navbar.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import {HiOutlineMenu,HiOutlineX} from "react-icons/hi"
+ 
+
+const Navbar = () => {
+
+    const [openSideMenu,seOpenSideMenu]=useState(false);
+
+  return (
+    <div className=''>
+        
+    </div>
+  )
+}
+
+export default Navbar
