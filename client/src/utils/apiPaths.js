3ad3e5b21701591d0f2d01446e90c6e793c2f314
Ironmac17 <nimish4agrawal@gmail.com>
2025-10-11 23:06:20 +0530

    v

--- client/src/utils/apiPaths.js
+++ client/src/utils/apiPaths.js
@@ -1,4 +1,5 @@
-// export const BASE_URL = import.meta.env.VITE_BASE_URL || ""; 
+export const BASE_URL = import.meta.env.VITE_BASE_URL || ""; 
+axios.post(`${BASE_URL}${API_PATHS.AUTH.LOGIN}`, data);
 
 export const API_PATHS={
     
