694f2d71c33fcf6e077aa80912a188a1f9767de9
Ironmac17 <nimish4agrawal@gmail.com>
2025-10-13 11:40:20 +0530

    m

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const connectDB = require("./config/db"); // adjust path if needed
+const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const incomeRoutes = require("./routes/incomeRoutes");
 const expenseRoutes = require("./routes/expenseRoutes");
@@ -19,23 +19,21 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => {
-    res.send("Helllo ")
-})
-
 app.use(express.json());
+
+// Database
 connectDB();
 
-// API routes
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello from the API!");
+});
+
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/income", incomeRoutes);
 app.use("/api/v1/expense", expenseRoutes);
 app.use("/api/v1/dashboard", dashboardRoutes);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-const PORT = process.env.PORT || 5000
-Sentry.setupExpressErrorHandler(app);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`)
-})
+module.exports = app;
